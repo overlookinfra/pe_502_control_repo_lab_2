b3ea59ba59e1a2dc96800fe57596e96ad37c9797
Jain Waldrip <jain.waldrip@perforce.com>
2024-04-04 13:37:40 -0500

    Remove webserver manifest from starting point

--- site-modules/role/manifests/webserver.pp
+++
@@ -1,7 +0,0 @@
-# Copyright 2024. Puppet, Inc., a Perforce company. 
-# 
-# This role is made of all the profiles that need to be included to make a webserver work
-# All roles should include the base profile
-class role::webserver {
-  include profile::base
-}
